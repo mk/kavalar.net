f7ce31d18b03923998623265cbb602077d9ea3d4
Martin Kavalar <martin@nextjournal.com>
2023-06-27 12:12:06 +0200

    Use simple-http-server

--- dev/user.clj
+++ dev/user.clj
@@ -1,5 +1,9 @@
 (ns user
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [nextjournal.clerk :as clerk]
+            [babashka.fs :as fs]))
 
 (clerk/serve! {:port 6655 :browse true})
 (clerk/show! "README.md")
+
+
+#_(.stop server 0)
